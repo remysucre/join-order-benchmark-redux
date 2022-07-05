25b101fb2f898a565ba6ebf9fdaea86b6c911439
WinstonBullen <winbullen@gmail.com>
2022-07-05 14:28:37 -0700

    Removing old joins

--- old_joins/3b.sql
+++
@@ -1,9 +0,0 @@
-SELECT MIN(t.title) AS movie_title
-FROM keyword AS k,
-     movie_info AS mi,
-     movie_keyword AS mk,
-     title AS t
-WHERE k.id = mk.keyword_id
-  AND t.id = mi.movie_id
-  AND t.id = mk.movie_id
-  AND mk.movie_id = mi.movie_id;
